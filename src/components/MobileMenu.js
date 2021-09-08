6ff2fd7eda17543319bbd1f4d8f302a752fa9dad
Kyle Garcia <kyle.fire@gmail.com>
2021-09-08 12:08:03 +0900

    created open and close function for hamburger menu

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,17 +1,26 @@
-export default function MobileMenu() {
-  return (
-    <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 modal'>
-      <div className='flex flex-col text-xl font-bold bg-white rounded-md w-80'>
-        <div className='py-6 pl-6'>
-          <h2>About</h2>
-        </div>
-        <div className='py-6 pl-6 border-t-2 border-b-2'>
-          <h2>Discover</h2>
-        </div>
-        <div className='py-6 pl-6'>
-          <h2>Get Started</h2>
+import { useState } from 'react';
+
+export default function MobileMenu(props) {
+  console.log(props);
+  if (props.opened === true)
+    return (
+      <div
+        onClick={props.closeMenu}
+        className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 modal'
+      >
+        <div className='flex flex-col mt-20 mb-auto text-xl font-bold bg-white rounded-md w-80'>
+          <div className='py-6 pl-6'>
+            <h2>About</h2>
+          </div>
+          <div className='py-6 pl-6 border-t-2 border-b-2'>
+            <h2>Discover</h2>
+          </div>
+          <div className='py-6 pl-6'>
+            <h2>Get Started</h2>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+
+  return <div></div>;
 }
